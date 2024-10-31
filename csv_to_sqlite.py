c0c16bd2e5f89e355ccdfda41f14179e144cbc55
OiliK <oilikalm@gmail.com>
2024-10-31 14:50:14 +0200

    vika

---
+++ csv_to_sqlite.py
@@ -0,0 +1,35 @@
+# import sqlite_connection as db # tuodan sqlite_connection eli virhe-homma ja aliaksella db
+import sqlite3
+import pandas as pd
+from pathlib import Path
+
+# Create empty file
+# Path("sql_leffat.db").touch()
+
+# Create db connection
+# connection = db.conn
+connection = sqlite3.connect("sql_leffat.db")
+
+# Create db cursor
+cursor = connection.cursor()
+
+# Create table with colums (only if table does not already exist)
+cursor.execute("CREATE TABLE IF NOT EXISTS movies \
+        (id INT, title TEXT, \
+        overview TEXT, \
+        popularity REAL, \
+        release_date TEXT, \
+        vote_average REAL, \
+        vote_count INT)")
+
+table_name = "elokuvat"
+# sivupolku.... >cursor.execute(f"SELECT * FROM {table_name}")
+
+# Load cursor data into panda dataframe
+movie_data = pd.read_csv("kokeilu.csv")
+
+# Write movie data into table. Index, ottaako rivinumerot vai ei
+movie_data.to_sql("elokuvat", connection, if_exists="append", index=False)
+
+#connection.commit()
+#connection.close()
--- main.py
+++ main.py
@@ -1,11 +1,59 @@
 import sqlite_connection as db # tuodan sqlite_connection eli virhe-homma ja aliaksella db
+import pandas as pd
+import csv
 connection = db.conn # connection on vain nimi, joka annetaan fileelle
 
 # JOMPI KUMPI TAPA, RIVITETTY TAI EI
 # rows = connection.execute("SELECT title, popularity FROM movies WHERE popularity >=20") 
+# cursor = connection.execute("SELECT title, popularity \
+#                           FROM movies \
+#                           WHERE popularity >=20") 
+
+# with db.conn as connection:
+#     cursor = connection.execute("SELECT title, popularity FROM movies WHERE popularity >=10") 
+
+# EKA RIVIN TULOSTUS:
+# result = cursor.fetchone() # >>> tällä ottaa eka rivin 
+# print(result)
+
+# ***************************************************
+# !) CSV fileen luominen PANDAS >>>> KOKEILU.CSV
+with db.conn as connection:
+    # Execute the query and read the result into a DataFrame
+    df = pd.read_sql("SELECT title, popularity FROM movies WHERE popularity >= 20", connection) # parametriksi yhteys=connection perään
+    
+    # Save the DataFrame to a CSV file
+    df.to_csv('kokeilu.csv', index=False)
+
+
+# 2) CSV fileen luominen CSV >>>>> LEFFAT.CSV
+# Open a CSV file to write the results
 cursor = connection.execute("SELECT title, popularity \
-                          FROM movies \
-                          WHERE popularity >=20") 
+                           FROM movies \
+                           WHERE popularity >=20") 
+# 1) Tallenna joka rivi listaan
+# 2) Tallenna listan data csv-tiedostoon csv-writerilla
+
+lista =[]
+for row in cursor: # fetchall - > palauttaa suoraan listan, ei tarvitse for -luuppia
+    lista.append(list(row))
+#print(lista)
+with open('leffat.csv', mode='w', encoding="utf-8", newline='') as file:
+    writer = csv.writer(file) # annetaan parametriksi file, jona tyhjä on perustettu
+    # Write all rows
+    writer.writerows(lista)
+
+
+# *******************************************************
+# CSV:n luku sisään näin PANDAS
+# leffat = pd.read_csv("kokeilu.csv")
+# print(leffat.head(5))
+
+# with open('kokeilu.csv', mode='r') as file:
+#     kokeilu = csv.reader(file)
+# kokeilu = pd.read_csv('kokeilu.csv')
+# print(kokeilu.head(5))
+
 
 # SELECT onnistuu ilman kursoria, mutta kursori on mekanisimi, joka mahdollistaa liikkumisen rivien ja kolumnien välillä (lukupää)
 # Pakko käyttää, kun resulttina palautuu
@@ -19,15 +67,14 @@ cursor = connection.execute("SELECT title, popularity \
 
 # print(rows[0]) >> ei toimi
 
-# result = cursor.fetchone() >>> tällä ottaa eka rivin 
-# print(result)
+
 
 # rivien hakeminen
-iteraattori = iter(cursor)
-rivi = next(iteraattori) # next siirtyy indexissä yhdellä eteepäin, HUOM! HAKEE VAIN kursoriin valitusta!!
-rivi2 = next(iteraattori)
-print(rivi2)
+# iteraattori = iter(cursor)
+# rivi = next(iteraattori) # next siirtyy indexissä yhdellä eteepäin, HUOM! HAKEE VAIN kursoriin valitusta!!
+# rivi2 = next(iteraattori)
+# print(rivi2)
+
 
 # suositellaan aina käyttämään
-connection.close()
-
+# connection.close()
